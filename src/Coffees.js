bf360fd86d78ff607798f285011a35cf179f73d9
Tess <tess.mueske@gmail.com>
2024-05-23 12:35:09 +0100

    fetching coffee data

--- src/Coffees.js
+++ src/Coffees.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Coffee from "./Coffee";
 import NewCoffee from "./NewCoffee";
 
 function Coffees() {
+const [coffees, setCoffees] = useState([])
+
+useEffect(() => {
+    fetch("http://localhost:3000/coffees")
+    .then(r => r.json())
+    .then(data => setCoffees(data))
+}, [])
+
     return (
         <div>
             <p>This is my coffees page!</p>
-            <Coffee />
+            {coffees.map((coffee) => <Coffee key={coffee.id} coffee={coffee}/>)}
             <NewCoffee />
         </div>
     );
