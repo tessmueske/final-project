d103fb5dd49082fc90a40fd5b2bc02090aebb223
Tess <tess.mueske@gmail.com>
2024-05-23 11:49:44 +0100

    Valid JSX change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from "./Home"
+import Home from "./Home";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 function App() {
   return (
+
     <div className="App">
       <h1>Hello, world!</h1>
     <Home />
     </div>
+
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Home(){
-    <div>
-        <p>Hello world! :D</p>
-    </div>
+    return (
+        <div>
+            <h2>Welcome to my page.</h2>
+        </div>
+    )
 }
 
 export default Home;
