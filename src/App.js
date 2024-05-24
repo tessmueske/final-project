5514db2fe23b84561311082cf895ec5c89bbca3c
Tess <tess.mueske@gmail.com>
2024-05-24 18:37:24 +0100

    outlet addition

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import './App.css';
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     < >
     <NavBar />
-    <div className="App">
-      <h1>thanks for being here!</h1>
+      <div className="App">
+      <Outlet />
     </div>
     </>
 
