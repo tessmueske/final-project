adaaa5c006bd329e06e3730ae4d1e1c341777197
Tess <tess.mueske@gmail.com>
2024-05-28 21:55:48 +0200

    about me edit

--- src/About.js
+++ src/About.js
@@ -3,7 +3,10 @@ import React from "react";
 function About(){
     return (
         <div>
-            <p>This project was created in May 2024 for Tess Mueske's final project in React.js.</p>
+            <h2>this is our global coffee index! here, you can read about various coffees from different areas of the world, that have been submitted by users. the information available includes harvesting period, tasting notes and aromas, as well as potential food pairings. you can add your own coffee to the global index using the link in our navigation bar above.</h2>
+            <br></br>
+            <br></br>
+            <p>this project was created in may 2024 for tess mueske's final project in react.js.</p>
         </div>
     )
 }
